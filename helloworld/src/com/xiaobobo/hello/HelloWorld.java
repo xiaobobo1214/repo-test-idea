0ad2fbbf4927edb6b77331591f90d0e1687e0e22
xiaobobo <994301552@qq.com>
2019-06-28 15:33:18 +0800

    第一层次推送到本地仓库

---
+++ helloworld/src/com/xiaobobo/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.xiaobobo.hello;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("hello git");
+    }
+}
